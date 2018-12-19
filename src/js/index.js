7de68c8c9060a85c54a7268302ee4c3c48b725bc
dandelionadia <nadia.ridko@gmail.com>
2018-12-19 17:20:55 +0100

    adds

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
-$(document).ready(function () {
-  // Create some JavaScript here
-});
+// var submenu = document.getElementsByClassName('menu__submenu');
+// submenu.addEventListener('click', function () {
+//   submenu.classList.toggle('open');
+// })
+
+var menuItems = document.querySelectorAll('.menu__item')
+
+menuItems.forEach(function (menuItem) {
+  menuItem.addEventListener('click', function (event) {
+    var clickedItem = event.target;
+    clickedItem.classList.toggle('menu__item--open');
+  })
+})
