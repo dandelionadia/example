ae94e82522c0c12d761860003cdae9bdc84cf0b7
dandelionadia <nadia.ridko@gmail.com>
2018-12-25 13:11:47 +0100

    removes menu items class click

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,17 @@ var menuItems = document.querySelectorAll('.menu__item')
 menuItems.forEach(function (menuItem) {
   menuItem.addEventListener('click', function (event) {
     var clickedItem = event.currentTarget;
-    clickedItem.classList.toggle('menu__item--open');
+    var isOpen = clickedItem.classList.contains('menu__item--open');
+    resetMenuClasses(menuItems);
+    if (!isOpen) {
+      clickedItem.classList.add('menu__item--open');
+    } 
     // debugger
   })
 })
+
+function resetMenuClasses(items) {
+  items.forEach(function(item) {
+    item.classList.remove('menu__item--open');
+  })
+}
