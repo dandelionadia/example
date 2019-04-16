44f4c60da35f25ebd246bcfa7039dfd66750fa8b
dandelionadia <nadia.ridko@gmail.com>
2019-04-16 16:01:40 +0200

    create carousel

--- src/js/index.js
+++ src/js/index.js
@@ -70,39 +70,37 @@ $(document).ready(function () {
     dots: false,
     adaptiveHeight: true
   });
-});
 
-$(document).ready(function () {
   $('.carousel-slides').slick({
     dots: true,
     infinite: false,
     speed: 300,
     slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      // {
-      //   breakpoint: 600,
-      //   settings: {
-      //     slidesToShow: 2,
-      //     slidesToScroll: 2
-      //   }
-      // },
-      // {
-      //   breakpoint: 480,
-      //   settings: {
-      //     slidesToShow: 1,
-      //     slidesToScroll: 1
-      //   }
-      // }
-    ]
+    slidesToScroll: 1,
+    // responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //       infinite: true,
+    //       dots: true
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1
+    //     }
+    //   }
+    // ]
   });
 });
