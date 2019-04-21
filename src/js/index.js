9d71bc8b60e93e470c3a7082b9d64dbe584ae3a3
dandelionadia <nadia.ridko@gmail.com>
2019-04-21 20:28:16 +0200

    adds responsive carousel

--- src/js/index.js
+++ src/js/index.js
@@ -73,34 +73,24 @@ $(document).ready(function () {
 
   $('.carousel-slides').slick({
     dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    // responsive: [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 3,
-    //       infinite: true,
-    //       dots: true
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 600,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 2
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1
-    //     }
-    //   }
-    // ]
+    // autoplay: true,
+    arrows: false,
+    adaptiveHeight: true,
+    speed: 600,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        }
+      }
+    ]
   });
 });
